f1219f7fc5ece969ce2f97ef4297025b5bc26fab
filippo-biondi <83963482+filippo-biondi@users.noreply.github.com>
2022-06-08 14:31:35 +0200

    Add files via upload

---
+++ common/json_gpu_loader.js
@@ -0,0 +1,41 @@
+var loadOnGPU = function(gl, jsonMesh) {
+   var gpuMesh = {
+    vertBuffer: null,
+    indexBufferTriangles: null
+   }
+   
+   gpuMesh.vertexBuffer = gl.createBuffer();
+   gpuMesh.normalBuffer = gl.createBuffer();
+   gpuMesh.indexBufferTriangles = gl.createBuffer();
+   
+   gl.bindBuffer( gl.ARRAY_BUFFER, gpuMesh.vertexBuffer);
+   gl.bufferData( 
+      gl.ARRAY_BUFFER, 
+	  new Float32Array(jsonMesh.vertices[0].values), 
+	  gl.STATIC_DRAW
+   );
+
+  gl.bindBuffer( gl.ARRAY_BUFFER, gpuMesh.normalBuffer);
+   gl.bufferData( 
+      gl.ARRAY_BUFFER, 
+	  new Float32Array(jsonMesh.vertices[1].values), 
+	  gl.STATIC_DRAW
+   );
+
+   if(jsonMesh.vertices[2] != undefined) {
+       gpuMesh.texCoordBuffer = gl.createBuffer();
+       gl.bindBuffer(gl.ARRAY_BUFFER, gpuMesh.texCoordBuffer);
+       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(jsonMesh.vertices[2].values), gl.STATIC_DRAW);
+   }
+
+   gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, gpuMesh.indexBufferTriangles );
+   gl.bufferData( 
+      gl.ELEMENT_ARRAY_BUFFER, 
+	  new Uint16Array(jsonMesh.connectivity[0].indices), 
+	  gl.STATIC_DRAW
+   );
+
+  gpuMesh.triangleIndices  = jsonMesh.connectivity[0].indices ; 
+ 
+   return gpuMesh;
+}
